e268782219a3893ecc1eb7df5b2dbc9b39f74253
Alysson Luis Martins Narloch <alysson.narloch@gmail.com>
2015-11-16 17:29:19 -0200

    Enviandoi

--- src/java/resource/CityResource.java
+++ src/java/resource/CityResource.java
@@ -13,8 +13,11 @@ import javax.ws.rs.core.GenericEntity;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriBuilder;
 import model.City;
+import model.Schedule;
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 @Path("city")
 public class CityResource {
@@ -55,5 +58,39 @@ public class CityResource {
         URI uriSelf = ub.build();
         
         return Response.ok(destination).link(uriSelf, "self").build();
+    }
+    
+    @GET
+    @Path("/city_name/{city_name}")
+    @Produces("application/json; charset=UTF-8")
+    public Response findByName(@PathParam("city_name") String cityName) {
+
+        SessionFactory sf = Util.getSessionFactory();
+        Session s = sf.openSession();
+        Transaction t = s.getTransaction();              
+        
+        try {
+            t.begin();
+
+            Query query = s.createQuery("FROM City WHERE city_name LIKE '%:cityName%'");
+            query.setString("cityName", cityName);
+
+            GenericEntity<List<City>> entity = new GenericEntity<List<City>>(query.list()) {
+            };
+
+            t.commit();
+
+            s.flush();
+            s.close();
+
+            return Response.ok(entity).build();
+        } catch (Exception ex) {
+            t.rollback();
+            
+            s.flush();
+            s.close();
+            
+            return Response.serverError().build();
+        }
     }
 }
