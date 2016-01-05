bf36dfea8421530a4cb331d1b76d47c65e3c1a04
Alysson Luis Martins Narloch <alysson.narloch@gmail.com>
2016-01-05 15:52:17 -0200

    Autenticação de acesso a API no lado do serviço da empresa aérea.

---
+++ src/java/auth/FilterAuthentication.java
@@ -0,0 +1,26 @@
+package auth;
+
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class FilterAuthentication implements ContainerRequestFilter {
+
+    public static final String AUTHENTICATION_HEADER = "Authorization";
+
+    @Override
+    public void filter(ContainerRequestContext containerRequest) throws WebApplicationException {
+        String authCredentials = containerRequest.getHeaderString(AUTHENTICATION_HEADER);
+
+        AuthenticationService authenticationService = new AuthenticationService();
+
+        boolean authenticationStatus = authenticationService.authenticate(authCredentials);
+
+        if (!authenticationStatus) {
+            throw new WebApplicationException(Status.UNAUTHORIZED);
+        }
+    }
+}
--- src/java/resource/ApplicationConfig.java
+++ src/java/resource/ApplicationConfig.java
@@ -14,6 +14,7 @@ public class ApplicationConfig extends Application {
     }
 
     private void addRestResourceClasses(Set<Class<?>> resources) {
+        resources.add(auth.FilterAuthentication.class);
         resources.add(resource.AuthenticationResource.class);
         resources.add(resource.CityResource.class);
         resources.add(resource.PurchaseResource.class);
